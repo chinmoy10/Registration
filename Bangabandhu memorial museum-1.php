7e52fc4b4636b3ea074eb86415958c0e3f2f5a1e
chinmoy10 <chinmoypodder22@gmail.com>
2021-01-08 17:27:50 +0600

    BBMMuseum

--- Bangabandhu memorial museum-1.php
+++ Bangabandhu memorial museum-1.php
@@ -102,7 +102,7 @@
             <h3>Location & Map</h3>
         </div>
     </section>
-    <section class="container map"> <iframe
+    <section class="container map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.879760936167!2d90.37423621471945!3d23.751666784588625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8ad7a8be543%3A0xba474ab92e9c23bc!2sFather%20of%20The%20Nation%20Bangabandhu%20Sheikh%20Mujibur%20Rahman%20Memorial%20Museum!5e0!3m2!1sen!2sbd!4v1605166679733!5m2!1sen!2sbd" width="1000" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> 
     </section>
 
